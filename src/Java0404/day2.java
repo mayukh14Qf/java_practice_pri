d32827322b076348b2537e5bfbe39a3b184fd5d8
ABC <mayukc55@gmail.com>
2024-04-07 13:06:00 +0530

    day 2

---
+++ src/Java0404/day2.java
@@ -0,0 +1,8 @@
+package Java0404;
+
+
+public class day2 {
+    public static void main(String[] args) {
+        System.out.println("hii there");
+    }
+}
---
+++ src/java0704/intro.java
@@ -0,0 +1,8 @@
+package java0704;
+import Java0404.*;
+public class intro {
+    public static void main(String[] args) {
+        // Comment ( code from here)
+        System.out.println("Mayukhjit Chakraborty");
+    }
+}
